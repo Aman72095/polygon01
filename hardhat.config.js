44cff8b12b583873bccb4ec1ad45ddd6e27b07c3
Aman72095 <amanpatel52268@gmail.com>
2024-08-14 17:08:02 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,14 +4,15 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
-  networks: {
-    mumbai: {
-      url: 'https://rpc-mumbai.maticvigil.com',
-      accounts: ["ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"],
+ networks: {
+    amoy: {
+      url: 'https://polygon-amoy.gateway.tatum.io',
+      accounts: ["11b0c9de64ab65df435387c8f2e2873d72cd7b719a4df372b6523a3079d363cc"],
     },
-    goerli: {
-      url: 'https://ethereum-goerli.publicnode.com',
-      accounts: ["2160702757ea47f08d7cbc21be51b0e6be8953dc3c912d9ab441fa9eae001021"],
+    sepolia: {
+      url: 'https://ethereum-sepolia.blockpi.network/v1/rpc/public',
+      accounts: ["3f7537940d0d1c2e21b0d7bd5496404b5d2b1c244df79404dd4933734d339673"],
     },
+    
   }
 };
